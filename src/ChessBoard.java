aad1fd0c50e4a254fbf1f74e6ff72d916cdb4c78
Benjimin Kick <gualyantoine@gmail.com>
2026-05-26 13:06:36 -0600

    Created the empty board and working on the Piece set up

--- src/ChessBoard.java
+++ src/ChessBoard.java
@@ -6,6 +6,28 @@ public class ChessBoard {
 
     public ChessBoard() {
         board = new ArrayList<ArrayList<String>>();
+        createEmptyBoard();
+
+    }
+
+    private void createEmptyBoard() {
+        for (int row = 0; row < 8; row++) {
+            ArrayList<String> currentRow = new ArrayList<String>();
+
+            for (int col = 0; col < 8; col++) {
+                currentRow.add(".");
+
+            }
+
+            board.add(currentRow);
+        }
+
+
+    }
+
+    private void setUpPieces() {
+        board.set(0, makeRow( new String[]{"r", "b", "n", "q", "k", "n", "b", "r"}));
+
     }
 
     
