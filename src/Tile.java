e560889a03ee76a85d529e1f7a7331c01bed9fbb
Benjimin Kick <gualyantoine@gmail.com>
2026-06-13 19:35:11 -0600

    Finished The first part of the Tile class. It basicly ask if the tile is occupied or not occupied and does things based on that condition

--- src/Tile.java
+++ src/Tile.java
@@ -1,14 +1,53 @@
 public abstract class Tile {
 
-    int tileCoordinate;
+    private int tileCoordinate;
 
     Tile(int tileCoordinate) {
         this.tileCoordinate = tileCoordinate;
     }
-
+    
+    // This method checks if the tile is Occupied 
     public abstract boolean isTileOccupied();
 
-    
+    // This method checks for which peiece is on the square
+    public abstract Piece getPiece();
+
+    public static final class EmptyTile extends Tile {
+        EmptyTile(int coordinate) {
+            super(coordinate);
+        }
+        
+        @Override
+        public boolean isTileOccupied() {
+            return false;
+        }
+
+        @Override 
+        public Piece getPiece() {
+            return null;
+        }
+    }
+
+
+    public static final class OccupiedTile extends Tile {
+        Piece pieceOnTile;
+
+        OccupiedTile(int tileCoordinate, Piece pieceOnTile) {
+            super(tileCoordinate);
+            this.pieceOnTile = pieceOnTile;
+        }
+
+        @Override
+        public boolean isTileOccupied() {
+            return true;
+        }
+
+        @Override
+        public Piece getPiece() {
+            return pieceOnTile;
+        }
+    }
+
 
     
 }
