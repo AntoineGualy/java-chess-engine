f3a5ea20ab0d11cbcdd567a5e010f4b2afb8b8bf
Benjimin Kick <gualyantoine@gmail.com>
2026-06-13 20:44:31 -0600

    Reorginized the code base and created the empty chess board

--- src/com/chess/engine/board/Tile.java
+++ src/com/chess/engine/board/Tile.java
@@ -1,6 +1,14 @@
+package com.chess.engine.board;
+
+import java.util.HashMap;
+import java.util.Map;
+// import com.google.common.collect.ImmutableMap;
+
+import com.chess.engine.pieces.Piece;
+
 public abstract class Tile {
 
-    private int tileCoordinate;
+    protected final int tileCoordinate;
 
     Tile(int tileCoordinate) {
         this.tileCoordinate = tileCoordinate;
@@ -9,11 +17,30 @@ public abstract class Tile {
     // This method checks if the tile is Occupied 
     public abstract boolean isTileOccupied();
 
+    private static final Map<Integer, EmptyTile> EMPTY_TILES = createAllPossibleEmptyTiles(); 
+
+    private static Map<Integer, EmptyTile> createAllPossibleEmptyTiles() {
+
+        final Map<Integer, EmptyTile> emptyTileMap = new HashMap<>();
+
+        for(int i = 0; i < 64; i++) {
+            emptyTileMap.put(i, new EmptyTile(i));
+        }
+        return Map.copyOf(emptyTileMap);
+    }
+
+    public static Tile createTile(final int tileCoordinate, final Piece piece) {
+        return piece != null ? new OccupiedTile(tileCoordinate, piece) : EMPTY_TILES.get(tileCoordinate);
+    }
+
+
     // This method checks for which peiece is on the square
     public abstract Piece getPiece();
 
+    
+
     public static final class EmptyTile extends Tile {
-        EmptyTile(int coordinate) {
+        EmptyTile(final int coordinate) {
             super(coordinate);
         }
         
@@ -30,7 +57,7 @@ public abstract class Tile {
 
 
     public static final class OccupiedTile extends Tile {
-        Piece pieceOnTile;
+        private final  Piece pieceOnTile;
 
         OccupiedTile(int tileCoordinate, Piece pieceOnTile) {
             super(tileCoordinate);
